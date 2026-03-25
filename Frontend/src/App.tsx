ebf02ed2c178a9d1adaf45460eb853260cfcc5cd
Derick Bessa de Almeida <derickbessa12@gmail.com>
2026-03-25 18:45:55 -0300

    feat: add useTheme hook, started adding ProductCard, added ProductProviders and the useTheme hook to the App.tsx

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,10 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import { ProductProvider } from './context/ProductContext'
+import { useTheme } from './hooks/useTheme'
 
 
 function App() {
+	const { isDark, toggleTheme } = useTheme();
 
+return(
+	<ProductProvider>
+		<></>
+	</ProductProvider>
+);
 }
 
 export default App
