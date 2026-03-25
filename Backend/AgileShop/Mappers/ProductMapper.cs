bca28781d269ba3eac1611a4fea8553999c00f47
Derick Bessa de Almeida <derickbessa12@gmail.com>
2026-03-25 00:22:14 -0300

    feat: add ProductMapper

---
+++ Backend/AgileShop/Mappers/ProductMapper.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AgileShop.DTOs;
+using AgileShop.Models;
+
+namespace AgileShop.Mappers
+{
+    public static class ProductMapper
+    {
+        public static ProductDto ToProductDto(this Product productModel)
+		{
+			return new ProductDto
+			{
+				Id = productModel.Id,
+				Name = productModel.Name,
+				Description = productModel.Description,
+				Price = productModel.Price,
+				Stock = productModel.Stock,
+				Category = productModel.Category,
+				ImageUrl = productModel.ImageUrl,
+				IsActive = productModel.IsActive,
+				CreatedAt = productModel.CreatedAt,
+			};
+		}
+		public static Product ToProductFromCreateDto(this CreateProductDto productDto)
+		{
+			return new Product
+			{
+				Name = productDto.Name,
+				Description = productDto.Description,
+				Price = productDto.Price,
+				Stock = productDto.Stock,
+				Category = productDto.Category,
+				ImageUrl = productDto.ImageUrl,
+			};
+		}
+    }
+}
--- Backend/AgileShop/obj/Debug/net10.0/AgileShop.AssemblyInfo.cs
+++ Backend/AgileShop/obj/Debug/net10.0/AgileShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgileShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c30adc73eb914a827bd24daab4e0b18a656da54b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eec25ff72bc50a086d6b0c75a4d8a177e56290b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgileShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgileShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
